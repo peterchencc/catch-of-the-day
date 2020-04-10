6854cd2a6546fe737cb713e9fa234605c9e7d6f7
peterchencc <peterchencc@gmail.com>
2020-04-10 16:00:09 +0800

    Basic layout and setState for fish form

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+class AddFishForm extends React.Component {
+  nameRef = React.createRef()
+  priceRef = React.createRef()
+  statusRef = React.createRef()
+  descRef = React.createRef()
+  imageRef = React.createRef()
+
+  createFish = (event) => {
+    event.preventDefault()
+
+    const fish = {
+      name: this.nameRef.current.value,
+      price: parseFloat(this.priceRef.current.value),
+      status: this.statusRef.current.value,
+      desc: this.descRef.current.value,
+      image: this.imageRef.current.value,
+    }
+
+    this.props.addFish(fish)
+    event.currentTarget.reset()
+  }
+
+  render() {
+    return (
+      <form className="fish-edit" onSubmit={this.createFish}>
+        <input type="text" name="name" ref={this.nameRef} placeholder="name" />
+        <input
+          type="text"
+          name="price"
+          ref={this.priceRef}
+          placeholder="price"
+        />
+        <select name="status" ref={this.statusRef}>
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold out!</option>
+        </select>
+        <input type="text" name="desc" ref={this.descRef} placeholder="desc" />
+        <input
+          type="text"
+          name="image"
+          ref={this.imageRef}
+          placeholder="image"
+        />
+        <button type="submit">+ Add fish</button>
+      </form>
+    )
+  }
+}
+
+export default AddFishForm
---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Header from './Header'
+import Order from './Order'
+import Inventory from './Inventory'
+
+class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {},
+  }
+
+  addFish = (fish) => {
+    console.log('adding a fish')
+    const fishes = { ...this.state.fishes }
+    fishes[`fish${Date.now()}`] = fish
+    this.setState({ fishes })
+  }
+
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+        </div>
+
+        <Order />
+        <Inventory addFish={this.addFish} />
+      </div>
+    )
+  }
+}
+
+export default App
