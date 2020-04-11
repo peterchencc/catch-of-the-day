eb4eacbba991ff42c2b0324c23400a1e1a04dd70
peterchencc <peterchencc@gmail.com>
2020-04-11 14:28:24 +0800

    Update Fish

--- src/components/App.js
+++ src/components/App.js
@@ -44,6 +44,12 @@ class App extends React.Component {
     this.setState({ fishes })
   }
 
+  updateFish = (key, updatedFish) => {
+    const fishes = { ...this.state.fishes }
+    fishes[key] = updatedFish
+    this.setState({ fishes })
+  }
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes })
   }
@@ -74,7 +80,9 @@ class App extends React.Component {
         <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
+          updateFish={this.updateFish}
           loadSampleFishes={this.loadSampleFishes}
+          fishes={this.state.fishes}
         />
       </div>
     )
---
+++ src/components/EditFishForm.js
@@ -0,0 +1,57 @@
+import React from 'react'
+
+class EditFishForm extends React.Component {
+  handleChange = (event) => {
+    const updatedFish = {
+      ...this.props.fish,
+      [event.currentTarget.name]: event.currentTarget.value,
+    }
+    console.log(updatedFish)
+    this.props.updateFish(this.props.index, updatedFish)
+  }
+
+  render() {
+    return (
+      <div className="fish-edit">
+        <input
+          type="text"
+          name="name"
+          value={this.props.fish.name}
+          onChange={this.handleChange}
+          placeholder="name"
+        />
+        <input
+          type="text"
+          name="price"
+          value={this.props.fish.price}
+          onChange={this.handleChange}
+          placeholder="price"
+        />
+        <select
+          name="status"
+          value={this.props.fish.status}
+          onChange={this.handleChange}
+        >
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold out!</option>
+        </select>
+        <input
+          type="text"
+          name="desc"
+          value={this.props.fish.desc}
+          onChange={this.handleChange}
+          placeholder="desc"
+        />
+        <input
+          type="text"
+          name="image"
+          value={this.props.fish.image}
+          onChange={this.handleChange}
+          placeholder="image"
+        />
+      </div>
+    )
+  }
+}
+
+export default EditFishForm
